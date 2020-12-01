f5af310f5a82733d6017ae2b3ebac10c75a5e5b8
Zac Petro <zmpetro@gmail.com>
2020-11-30 18:46:30 -0600

    initial recommender file

---
+++ recommender.py
@@ -0,0 +1,8 @@
+# Import AL/ML libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import mean_squared_error
+
+print("Hello World!")
