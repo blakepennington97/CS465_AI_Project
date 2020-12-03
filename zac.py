da34f7d3336f75751cc2ee34c7d53fd9ccaaca9a
Zac Petro <zmpetro@gmail.com>
2020-12-02 21:59:41 -0600

    intial data formatting & visualization

--- zac.py
+++ zac.py
@@ -5,10 +5,50 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import mean_squared_error
 
+# pd.set_option("display.max_rows", None, "display.max_columns", None)
+
 # Import the movies dataset
 movies = pd.read_csv('ml-latest-small/movies.csv')
-print(movies.head())
+#print(movies.head())
 
 # Import the movie ratings dataset
 ratings = pd.read_csv('ml-latest-small/ratings.csv')
-print(ratings.head())
+#print(ratings.head())
+
+# How many movies and ratings are there?
+print('The dataset contains', len(ratings), 'ratings of', len(movies), 'movies.')
+
+# Compare preferences of romance vs. comedy movies
+def compare_genre_tastes(movies, ratings, genres, columns):
+    gt = pd.DataFrame()
+    for genre in genres:
+        genre_movies = movies[movies['genres'].str.contains(genre)]
+        avg_genre_rating = ratings[ratings['movieId'].isin(genre_movies['movieId'])].loc[:, ['userId', 'rating']].groupby(['userId'])['rating'].mean().round(2)
+        
+        gt = pd.concat([gt, avg_genre_rating], axis=1)
+        
+    gt.columns = columns
+    return gt
+
+gt = compare_genre_tastes(movies, ratings, ['Romance', 'Comedy'], ['avg_romance_rating', 'avg_comedy_rating'])
+# print(gt.head())
+
+# Curate the genre tastes so that the only users within it like 
+# either romance or comedy movies (rating for either is above 3.0)
+gt = gt.drop(gt[(gt.avg_romance_rating <= 3.0) & (gt.avg_comedy_rating <= 3.0)].index)
+# Remove rows with null ratings
+gt = gt.dropna(how='any', axis=0)
+gt.info()
+
+# Visualize the data
+def draw_scatterplot(x_data, x_label, y_data, y_label):
+    plot = plt.figure(figsize=(8,8))
+    ax = plot.add_subplot(111)
+    plt.xlim(0, 5)
+    plt.ylim(0, 5)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.scatter(x_data, y_data, s=30)
+    plt.show()
+
+# draw_scatterplot(gt['avg_romance_rating'], 'Avg romance rating', gt['avg_comedy_rating'], 'Avg comedy ratings')
