9064f658706d112818d7691f0b288104b1000a7a
Zac Petro <zmpetro@gmail.com>
2020-12-04 16:01:59 -0600

    make code more modular

--- zac.py
+++ zac.py
@@ -18,6 +18,11 @@ ratings = pd.read_csv('ml-latest-small/ratings.csv')
 # How many movies and ratings are there?
 print('The dataset contains', len(ratings), 'ratings of', len(movies), 'movies.')
 
+genre1 = 'Romance'
+xlabel = 'Avg ' + genre1 + ' rating'
+genre2 = 'Horror'
+ylabel = 'Avg ' + genre2 + ' rating'
+
 # Compare preferences of romance vs. comedy movies
 def compare_genre_tastes(movies, ratings, genres, columns):
     gt = pd.DataFrame()
@@ -30,12 +35,12 @@ def compare_genre_tastes(movies, ratings, genres, columns):
     gt.columns = columns
     return gt
 
-gt = compare_genre_tastes(movies, ratings, ['Romance', 'Comedy'], ['avg_romance_rating', 'avg_comedy_rating'])
+gt = compare_genre_tastes(movies, ratings, [genre1, genre2], ['avg_genre1_rating', 'avg_genre2_rating'])
 # print(gt.head())
 
 # Curate the genre tastes so that the only users within it like 
 # either romance or comedy movies (rating for either is above 3.0)
-gt = gt.drop(gt[(gt.avg_romance_rating <= 3.0) & (gt.avg_comedy_rating <= 3.0)].index)
+gt = gt.drop(gt[(gt.avg_genre1_rating <= 2.5) & (gt.avg_genre2_rating <= 2.5)].index)
 # Remove rows with null ratings
 gt = gt.dropna(how='any', axis=0)
 gt.info()
@@ -51,12 +56,12 @@ def draw_scatterplot(x_data, x_label, y_data, y_label):
     ax.scatter(x_data, y_data, s=25)
     plt.show()
 
-# draw_scatterplot(gt['avg_romance_rating'], 'Avg romance rating', gt['avg_comedy_rating'], 'Avg comedy rating')
+draw_scatterplot(gt['avg_genre1_rating'], xlabel, gt['avg_genre2_rating'], ylabel)
 
 # Use K-means to break the data down into hopefully 3 groups
-# 1. People who like romance, but not comedy
-# 2. People who like comedy, but not romance
-# 3. People who like both romance and comedy
+# 1. People who like genre1, but not genre2
+# 2. People who like genre2, but not genre1 
+# 3. People who like both genre1 and genre2
 
 k_means = KMeans(init="random", n_clusters=3, random_state=99)
 labels = k_means.fit_predict(gt)
@@ -66,10 +71,10 @@ def plot_kmeans(gt, labels):
     ax = plot.add_subplot(111)
     plt.xlim(0, 5)
     plt.ylim(0, 5)
-    ax.set_xlabel('Avg romance rating')
-    ax.set_ylabel('Avg comedy rating')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
 
-    ax.scatter(x=gt['avg_romance_rating'], y=gt['avg_comedy_rating'], c=labels, s=25, cmap='viridis')
+    ax.scatter(x=gt['avg_genre1_rating'], y=gt['avg_genre2_rating'], c=labels, s=25, cmap='viridis')
 
     plt.show()
 
